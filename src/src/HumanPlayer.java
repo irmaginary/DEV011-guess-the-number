3232b9cb39b991c573af074bccbf2a2e341c2834
irmasolano <i.solano.baca@gmail.com>
2024-01-15 23:58:13 -0600

    Ajustes de forma y bienvenida el juego

--- src/src/HumanPlayer.java
+++ src/src/HumanPlayer.java
@@ -19,7 +19,9 @@ public class HumanPlayer extends Player {
 
     // Método adicional para obtener el nombre del jugador
     public void enterName() {
-        System.out.println("Enter your name: ");
+        System.out.println("*** Welcome to Guess The number ***");
+        System.out.println("Guess a number between 1 and 100 and try to win vs the Computer just for fuuuuun!:D");
+        System.out.println("First things first, Enter your name: ");
         String playerName = scanner.nextLine();
         setName(playerName);
     }
