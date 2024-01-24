c7b9b1bbaf62f9a814e45516b8f7973005a3a2ec
irmasolano <i.solano.baca@gmail.com>
2024-01-23 23:50:36 -0600

    primer test

--- src/test/GuessTheNumberGameTest.java
+++ src/test/GuessTheNumberGameTest.java
@@ -1,21 +1,31 @@
 import org.junit.jupiter.api.Test;
-
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import static org.mockito.Mockito.*;
 
 class GuessTheNumberGameTest {
-    @Test
-    public void test() {
-
-        assertEquals(1,1);
-    }
 
     @Test
-    public void testCheckGuess(){
-    GuessTheNumberGame guessTheNumberGame = mock(GuessTheNumberGame.class);
-    when(guessTheNumberGame.checkGuess()).thenReturn();
+    public void testCheckGuess() {
+        // Mockear las dependencias de la clase
+        HumanPlayer humanPlayerMock = mock(HumanPlayer.class);
+        ComputerPlayer computerPlayerMock = mock(ComputerPlayer.class);
+
+        int targetNumber = 40;
+
+        // Comportamiento esperado
+        when(humanPlayerMock.getName()).thenReturn("Irma");
+        when(humanPlayerMock.makeGuess(any())).thenReturn(89, 45, 42, 40);
+        when(computerPlayerMock.makeGuess(any())).thenReturn(43);
+
+        // Crear instancia de GuessTheNumberGame
+        GuessTheNumberGame game = new GuessTheNumberGame();
+
+        // Ejecutar método con los mocks
+        game.playGame(humanPlayerMock, computerPlayerMock, targetNumber);
+
+        verify(humanPlayerMock, atLeastOnce()).makeGuess(computerPlayerMock);
+        verify(computerPlayerMock, atLeastOnce()).makeGuess(humanPlayerMock);
     }
 }
+
 
