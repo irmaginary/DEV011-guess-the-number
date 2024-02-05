21f41571fc71a496edd510951a6ab6ec9f92babf
irmasolano <i.solano.baca@gmail.com>
2024-02-05 16:43:15 -0600

    prueba

--- src/src/Player.java
+++ src/src/Player.java
@@ -10,36 +10,7 @@ public abstract class Player {
     getName(): string;
     getGuesses(): Array<int>;
 }*/
-/*import java.util.ArrayList;
-import java.util.List;
 
-//esta clase no se instanci, se hereda a HumanPlayer y ComputerPlayer
-public abstract class Player {
-    // Atributos
-    private String name;
-    private List<Integer> guesses;
-
-    // Constructor
-    public Player(String name) {
-        this.name = name;
-        this.guesses = new ArrayList<>();
-    }
-
-    // Método abstracto para hacer una suposición
-    public abstract int makeGuess();
-
-    // Método para obtener el nombre
-    public String getName() {
-        return name;
-    }
-
-    // Método para obtener el historial de suposiciones
-    public List<Integer> getGuesses(int guess) {
-        return guesses;
-    }
-
-
-}*/
 import java.util.Scanner;
 import java.util.ArrayList;
 public abstract class Player {
